152f94128c7a71a40de6869ff46ba57c1129278e
alifanantomi <alifanantomi2@gmail.com>
2024-10-30 13:35:30 +0700

    init

---
+++ components/button.vue
@@ -0,0 +1,5 @@
+<template>
+  <button 
+    class="p-2 bg-red-600 text-white rounded-lg"
+  >Click Me 2!</button>
+</template>
---
+++ components/header.vue
@@ -0,0 +1,3 @@
+<template>
+  <header>Header Components</header>
+</template>
