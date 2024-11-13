bb717e54ebee929d80afea6b4f0a740c8de5483b
alifanantomi <alifanantomi2@gmail.com>
2024-11-13 15:23:24 +0700

    get list products

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,30 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   css: ['~/asset/css/main.css'],
+  modules: ['@nuxtjs/supabase'],
+  supabase: {
+    redirectOptions: {
+      login: '/login',
+      callback: '/confirm',
+      include: undefined,
+      exclude: [
+        '/', 
+        '/detail/*', 
+        '/cart', 
+        '/register', 
+        '/dashboard/*',
+        '/dashboard'
+      ],
+      cookieRedirect: false,
+    }
+  }
 })
