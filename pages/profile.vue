f68b6cb28097ba34ef3e961f16a9b09521367315
alifanantomi <alifanantomi2@gmail.com>
2024-11-20 11:23:34 +0700

    add login register

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,14 +1,57 @@
+<script setup lang="ts">
+const client = useSupabaseClient()
+
+const user = useSupabaseUser()
+const name = ref('')
+const loading = ref(true)
+
+if (user.value) {
+  const { data } = await client
+    .from('profiles')
+    .select('name')
+    .eq('id', user.value.id)
+    .single()
+  
+  if (data) {
+    name.value = data.name
+  }
+  
+}
+
+loading.value = false
+
+async function signOut() {
+  try {
+    loading.value = true
+    if (confirm('Are you sure you want to logout?')) {
+      const { error } = await client.auth.signOut()
+      if (error) throw error
+      user.value = null
+
+      navigateTo('/login')
+    } else {
+      return null
+    }
+  } catch (error: any) {
+    alert(error.message)
+  } finally {
+    loading.value = false
+  }
+}
+
+</script>
+
 <template>
   <div class="w-full">
     <div class="flex flex-col w-[480px] shadow-lg rounded-lg p-6 mx-auto gap-4">
       <div class="flex flex-col gap-1">
         <h1 class="text-2xl font-semibold">
-          Hello, Djatmiko
+          Hello, {{ name }}
         </h1>
-        <span class="text-lg">djatmiko@mail.com</span>
+        <span class="text-lg">{{ user?.email }}</span>
       </div>
 
-      <button class="min-w-[120px] text-lg bg-blue-500 text-white p-2 rounded-lg">
+      <button class="min-w-[120px] text-lg bg-blue-500 text-white p-2 rounded-lg" @click="signOut">
         Logout
       </button>
     </div>
